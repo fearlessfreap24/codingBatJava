51994550196769c4eca44e19d08d6240c9508ac6
dylanptcu <dylan.perez@tcu.edu>
2019-08-26 01:41:37 -0500

    added original description

--- NearHundred.java
+++ NearHundred.java
@@ -1,3 +1,6 @@
+// Given an int n, return true if it is within 10 of 100 or 200.
+// Note: Math.abs(num) computes the absolute value of a number.
+
 public class NearHundred {
 
     public static void main(String[] args){
