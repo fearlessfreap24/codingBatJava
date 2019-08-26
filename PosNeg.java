89947d3f83e61242ae1eff7ba80abbc722ab8590
dylanptcu <dylan.perez@tcu.edu>
2019-08-26 01:57:27 -0500

    added description

--- PosNeg.java
+++ PosNeg.java
@@ -1,3 +1,6 @@
+//Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true,
+// then return true only if both are negative.
+
 public class PosNeg {
 
     public static void main(String[] args){
